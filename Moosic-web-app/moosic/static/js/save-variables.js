9bbbb35fd7b992e58946116f21c25e32a2217c78
ShahiW <s.winderling@posteo.de>
2023-09-07 10:02:03 +0200

    delete placeholder links in blobs

---
+++ Moosic-web-app/moosic/static/js/save-variables.js
@@ -0,0 +1,9 @@
+function save_mood() {
+    var input = document.getElementById("userInputMood").value;
+    alert(input);
+}
+
+function save_genre() {
+    var input = document.getElementById("userInputGenre").value;
+    alert(input);
+}
--- Moosic-web-app/moosic/views.py
+++ Moosic-web-app/moosic/views.py
@@ -21,6 +21,10 @@ class MoosicViews(BaseView):
     def choose_genre_mood(self):
         return render_template('choose-genre-mood.html', genres=self.genres)
     
+    @expose('/results')
+    def results(self):
+        return render_template('results.html')
+    
 
     @expose('/list/')
     def list(self):
