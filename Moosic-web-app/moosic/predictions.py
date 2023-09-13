7b5087ec7ae7a4d6ced91cfa02de5d524451d672
ShahiW <s.winderling@posteo.de>
2023-09-13 09:25:35 +0200

    delete interim and change to results

--- Moosic-web-app/moosic/predictions.py
+++ Moosic-web-app/moosic/predictions.py
@@ -3,6 +3,6 @@
 # dummy function
 def get_results(model, genre: str, mood: str): 
     # make dummy list (artist,song)
-    tracks = [{'artist': 'Disturbed', 'name': "Down with the Sickness", 'id': 'aw35sredcftvgzbhunjik'}]
+    tracks = [{'artist': 'Disturbed', 'name': "Down with the Sickness", 'id': '1jJxMXKtkD9weHJxgyXV0j'}]
     # shows 5 tracks
     return tracks
